a8e8fce227ca844e0a1edfe730b5100fe65fa73f
Ardiansyah <101657545+ansyahh@users.noreply.github.com>
2022-11-08 22:15:15 -0800

    update

---
+++ abstrackBuku.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class abstrackBuku here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public abstract class abstrackBuku
+{
+    // instance variables - replace the example below with your own
+  
+
+    /**
+     * Constructor for objects of class abstrackBuku
+     */
+    public abstract void available();
+    
+}
---
+++ buku.java
@@ -0,0 +1,25 @@
+public class buku extends abstrackBuku{
+    private String judul;
+    private int kodeBuku;
+    private String pengarang;
+    
+    public buku(String judul,int kodeBuku,String pengarang){
+
+        this.judul = judul;
+        this.kodeBuku=kodeBuku;
+        this.pengarang=pengarang;
+
+    }
+    public String getjudul(){
+        return this.judul;
+    }
+    public int getkodeBuku(){
+        return this.kodeBuku;
+    }
+    public String getpengarang(){
+        return this.pengarang;
+    }
+    public void available(){
+        
+    }
+}
